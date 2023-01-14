749ecb09c63c02d9563c838b122f345db020c20e
zhifenghao123 <zhifenghao123@outlook.com>
2023-01-14 16:40:54 +0800

    新增实现类

--- how-dev-biz/src/main/java/com/howdev/app/api/DecisionApiImpl.java
+++ how-dev-biz/src/main/java/com/howdev/app/api/DecisionApiImpl.java
@@ -1,8 +1,30 @@
-package com.howdev.app.api;/**
+package com.howdev.app.api;
+
+import com.howdev.api.model.BaseRequest;
+import com.howdev.api.model.BaseResponse;
+import com.howdev.api.model.DecisionRequest;
+import com.howdev.api.model.DecisionResponse;
+import com.howdev.api.provider.DecisionApi;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+/**
  * DecisionApiImpl class
  *
  * @author haozhifeng
  * @date 2023/01/14
  */
-public class DecisionApiImpl {
+@Service
+public class DecisionApiImpl implements DecisionApi {
+    @Override
+    public BaseResponse<DecisionResponse> syncDecision(@NotNull(message = "decisionRequest is null") @Valid BaseRequest<DecisionRequest> decisionRequest) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse<DecisionResponse> asyncDecision(@NotNull(message = "decisionRequest is null") @Valid BaseRequest<DecisionRequest> decisionRequest) {
+        return null;
+    }
 }
