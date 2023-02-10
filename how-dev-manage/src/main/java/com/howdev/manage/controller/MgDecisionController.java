aac9b9e43dfc2feccebbe31aedeb1c789dabeddb
zhifenghao123 <zhifenghao123@outlook.com>
2023-02-10 17:08:28 +0800

    增加Controller

--- how-dev-manage/src/main/java/com/howdev/manage/controller/MgDecisionController.java
+++ how-dev-manage/src/main/java/com/howdev/manage/controller/MgDecisionController.java
@@ -1,8 +1,25 @@
-package com.howdev.manage.controller;/**
+package com.howdev.manage.controller;
+
+import com.howdev.manage.entity.MgDecisionInfo;
+import com.howdev.manage.vo.BaseResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
  * MgDecisionController class
  *
  * @author haozhifeng
  * @date 2023/02/10
  */
+@Controller
+@RequestMapping("decision/manage")
 public class MgDecisionController {
+
+    @ResponseBody
+    @RequestMapping(value = "create")
+    public BaseResponse create(@RequestBody MgDecisionInfo mgDecisionInfo) {
+        return BaseResponse.newSuccResponse(null);
+    }
 }
