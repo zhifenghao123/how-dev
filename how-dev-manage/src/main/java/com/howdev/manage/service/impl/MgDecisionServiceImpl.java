e606e2eed1a3249f4686730f860f3c8e9528a4cb
zhifenghao123 <zhifenghao123@outlook.com>
2023-02-12 16:00:45 +0800

    增加决策管理服务类

--- how-dev-manage/src/main/java/com/howdev/manage/service/impl/MgDecisionServiceImpl.java
+++ how-dev-manage/src/main/java/com/howdev/manage/service/impl/MgDecisionServiceImpl.java
@@ -1,8 +1,12 @@
 package com.howdev.manage.service.impl;
 
 import com.howdev.manage.entity.MgDecisionInfo;
+import com.howdev.manage.mapper.MgDecisionInfoMapper;
 import com.howdev.manage.service.MgDecisionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * MgDecisionServiceImpl class
@@ -12,8 +16,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MgDecisionServiceImpl implements MgDecisionService {
+    private MgDecisionInfoMapper mgDecisionInfoMapper;
+
+    @Autowired
+    public void setMgDecisionInfoMapper(MgDecisionInfoMapper mgDecisionInfoMapper) {
+        this.mgDecisionInfoMapper = mgDecisionInfoMapper;
+    }
+
     @Override
     public boolean create(MgDecisionInfo mgDecisionInfo) {
-        return false;
+        Date current = new Date();
+        String operator = "haozhifeng";
+        mgDecisionInfo.setCreateTime(current);
+        mgDecisionInfo.setUpdateTime(current);
+        mgDecisionInfo.setCreator(operator);
+        mgDecisionInfo.setModifier(operator);
+        int insert = mgDecisionInfoMapper.insert(mgDecisionInfo);
+        return insert == 1;
     }
 }
