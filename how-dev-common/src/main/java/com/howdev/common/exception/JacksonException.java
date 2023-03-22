efa1d3920d57041926afd623666b9604724c8eb3
zhifenghao123 <zhifenghao123@outlook.com>
2023-03-22 14:46:51 +0800

    新增JacksonException

--- how-dev-common/src/main/java/com/howdev/common/exception/JacksonException.java
+++ how-dev-common/src/main/java/com/howdev/common/exception/JacksonException.java
@@ -1,8 +1,17 @@
-package com.howdev.common.exception;/**
+package com.howdev.common.exception;
+
+/**
  * JacksonException class
  *
  * @author haozhifeng
  * @date 2023/03/21
  */
-public class JacksonException {
+public class JacksonException extends RuntimeException {
+    public JacksonException() {
+        // no need to do.
+    }
+
+    public JacksonException(String message) {
+        super(message);
+    }
 }
