e535aa45a4134b47b8c3b4b87dcb07105389f48c
zhifenghao123 <zhifenghao123@outlook.com>
2023-06-01 10:01:33 +0800

    测试emoji序列化处理

--- how-dev-common/src/main/java/com/howdev/common/TestMain.java
+++ how-dev-common/src/main/java/com/howdev/common/TestMain.java
@@ -1,8 +1,79 @@
-package com.howdev.common;/**
+package com.howdev.common;
+
+import com.howdev.common.testmodel.RequestData;
+import com.howdev.common.util.EmojiUtil;
+import com.howdev.common.util.JacksonUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
+
+/**
  * TestMain class
  *
  * @author haozhifeng
  * @date 2023/03/21
  */
 public class TestMain {
+    public static void main(String[] args) {
+        String str =
+        "{\n" +
+                "    \"type\":\"async\",\n" +
+                "    \"reqId\":\"1111111111\",\n" +
+                "    \"params\":{\n" +
+                "        \"kuaishou_loan_user_id\":\"465340806850291026\",\n" +
+                "        \"seller_type\":\"\\u4e2a\\u4eba\\u5356\\u5bb6\",\n" +
+                "        \"seller_name\":\"\\u5c1a\\u5251\\u51ef\",\n" +
+                "        \"shop_name\":\"KILLER\\\\\\ud83e\\udd66\\u7684\\u5c0f\\u5e97\",\n" +
+                "        \"shop_score\":\"4.50\",\n" +
+                "        \"shop_rank\":\"\\u4e2d\",\n" +
+                "        \"shop_status\":\"\\u6b63\\u5e38\"\n" +
+                "    }\n" +
+                "}";
+
+
+        //String str2 = readFileContent("../../resources/testfile.txt");
+
+
+
+        RequestData requestData = JacksonUtil.toObject(str, RequestData.class);
+
+        System.out.println(requestData);
+
+        Map emojiReplaceMap = EmojiUtil.emojiReplaceMap(JacksonUtil.toJson(requestData.getParams()));
+
+        System.out.println(emojiReplaceMap);
+
+
+    }
+
+    public static String readFileContent(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        StringBuffer sbf = new StringBuffer();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempStr;
+            while ((tempStr = reader.readLine()) != null) {
+                sbf.append(tempStr);
+            }
+            reader.close();
+            return sbf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return sbf.toString();
+    }
 }
--- how-dev-common/src/main/java/com/howdev/common/testmodel/RequestData.java
+++ how-dev-common/src/main/java/com/howdev/common/testmodel/RequestData.java
@@ -1,8 +1,30 @@
-package com.howdev.common.testmodel;/**
+package com.howdev.common.testmodel;
+
+import java.util.Map;
+
+/**
  * RequestData class
  *
  * @author haozhifeng
  * @date 2023/03/21
  */
 public class RequestData {
+    private String reqId;
+    private Map<String, Object> params;
+
+    public String getReqId() {
+        return reqId;
+    }
+
+    public void setReqId(String reqId) {
+        this.reqId = reqId;
+    }
+
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
+    }
 }
