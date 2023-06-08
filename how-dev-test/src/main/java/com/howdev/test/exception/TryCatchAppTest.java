517e378f18d1f61c1748fe127a5b938000291d5a
zhifenghao123 <zhifenghao123@outlook.com>
2023-06-08 11:46:50 +0800

    新增how-dev-test

---
+++ how-dev-test/src/main/java/com/howdev/test/exception/TryCatchAppTest.java
@@ -0,0 +1,73 @@
+package com.howdev.test.exception;
+
+/**
+ * TryCatchAppTest class
+ *
+ * @author haozhifeng
+ * @date 2023/06/08
+ */
+public class TryCatchAppTest {
+    public static void main(String[] args) {
+        tryCatchFinallyWithoutReturn();
+        System.out.println("------------------------");
+        System.out.println(tryCatchFinallyWithReturn1());
+        System.out.println("-----------------------");
+        System.out.println(tryCatchFinallyWithReturn2());
+
+
+    }
+
+    public static void tryCatchFinallyWithoutReturn() {
+        int i = 0;
+        try {
+            System.out.println("start execute try code clock.");
+            i = 1 / i;
+            System.out.println("end execute try code clock.");
+
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("execute catch nullPointerException code clock.");
+        } catch (Exception exception) {
+            System.out.println("execute catch exception code clock.");
+        } finally {
+            System.out.println("execute finally code clock.");
+        }
+    }
+
+    public static int tryCatchFinallyWithReturn1() {
+        int i = 0;
+        try {
+            System.out.println("start execute try code clock.");
+            i = 1 / i;
+            System.out.println("end execute try code clock.");
+            return i;
+
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("execute catch nullPointerException code clock.");
+            return -999;
+        } catch (Exception exception) {
+            System.out.println("execute catch exception code clock.");
+            return -998;
+        } finally {
+            System.out.println("execute finally code clock.");
+            return -100;
+        }
+    }
+
+    public static int tryCatchFinallyWithReturn2() {
+        int i = 0;
+        try {
+            System.out.println("start execute try code clock.");
+            i = 1 / i;
+            System.out.println("end execute try code clock.");
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("execute catch nullPointerException code clock.");
+            return -999;
+        } catch (Exception exception) {
+            System.out.println("execute catch exception code clock.");
+            return -998;
+        } finally {
+            System.out.println("execute finally code clock.");
+        }
+        return i;
+    }
+}
