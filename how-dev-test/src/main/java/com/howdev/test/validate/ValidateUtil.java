60f731976bb6e3fc45b505e66aadfacecbcf472f
zhifenghao123 <zhifenghao123@outlook.com>
2023-06-15 09:57:35 +0800

    新增校验工具

--- how-dev-test/src/main/java/com/howdev/test/validate/ValidateUtil.java
+++ how-dev-test/src/main/java/com/howdev/test/validate/ValidateUtil.java
@@ -1,8 +1,30 @@
-package com.howdev.test.validate;/**
+package com.howdev.test.validate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
  * ValidateUtil class
  *
  * @author haozhifeng
  * @date 2023/06/14
  */
 public class ValidateUtil {
+    public static void checkNullWithError(Object object, Object... args) throws Exception {
+        if (object == null) {
+            throw new Exception();
+        }
+        /*if (object instanceof CharSequence && StringUtils.isBlank((CharSequence) object)) {
+            throw new Exception();
+        }*/
+        if (object instanceof Collection && ((Collection)object).size() == 0) {
+            System.out.println("0000");
+            throw new Exception();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //checkNullWithError(null);
+        checkNullWithError(new ArrayList<Integer>());
+    }
 }
