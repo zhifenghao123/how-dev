63a668ca8fc43f7f1c8854748e9fe2b38faa0946
zhifenghao123 <zhifenghao123@outlook.com>
2023-09-09 23:32:18 +0800

    增加编码和文件操作测试

--- how-dev-test/src/main/java/com/howdev/test/business/controller/UserController.java
+++ how-dev-test/src/main/java/com/howdev/test/business/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.howdev.test.business.controller;
 
+import com.howdev.common.util.FileReadUtil;
+import com.howdev.common.util.FileWriteUtil;
 import com.howdev.common.util.JacksonUtil;
 import com.howdev.test.business.component.CalculateProcessor1;
 import com.howdev.test.business.entity.User;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 import java.util.List;
 
 /**
@@ -58,5 +63,88 @@ public class UserController {
         return "OK! \n\r reuqest type = " + type + "\n\n process method = " + method;
 
     }
+
+
+
+    @ResponseBody
+    @RequestMapping(value="decodeTest")
+    public String decodeTest() {
+        User user = new User();
+        user.setId(00000);
+        user.setName("我我我我");
+        user.setBirthDate("1997-04-04");
+        user.setGender(0);
+        String userJson = JacksonUtil.toJson(user);
+        String base64Encode = getBase64Encode(userJson);
+        String base64Decode = getBase64Decode(base64Encode);
+        System.out.println(base64Encode);
+        System.out.println("---");
+        System.out.println(base64Decode);
+        return base64Decode;
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value="decodeTestWithReadFile")
+    public String decodeTestWithReadFile() {
+        User user = new User();
+        user.setId(00000);
+        user.setName("我我我我");
+        user.setBirthDate("1997-04-04");
+        user.setGender(0);
+        String userJson = JacksonUtil.toJson(user);
+        String base64Encode = getBase64Encode(userJson);
+        String fileName = "./how-dev-test/data_dir/person-2.txt";
+        try {
+            FileWriteUtil.writeToFileWithFiles(fileName,base64Encode);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        String fileContent = null;
+        try {
+            fileContent = FileReadUtil.readByByteArray(fileName);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        String base64Decode = getBase64Decode(fileContent);
+        System.out.println(base64Encode);
+        System.out.println("---");
+        System.out.println(base64Decode);
+        return base64Decode;
+
+    }
+
+    // 加密
+    public static String getBase64Encode(String originStr) {
+        byte[] bytes = null;
+        String encodedDataString = null;
+        try {
+            bytes = originStr.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        if (bytes != null) {
+            encodedDataString = Base64.getMimeEncoder().encodeToString(bytes);
+        }
+        return encodedDataString;
+    }
+
+    // 解密
+    public static String getBase64Decode(String encodedDataString) {
+        byte[] bytes = null;
+        String decodedDataString = null;
+        if (encodedDataString != null) {
+            try {
+                bytes = Base64.getMimeDecoder().decode(encodedDataString);
+                decodedDataString = new String(bytes, "utf-8");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return decodedDataString;
+    }
+
+
 
 }
