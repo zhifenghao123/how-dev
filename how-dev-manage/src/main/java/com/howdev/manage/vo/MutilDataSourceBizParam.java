63cdaa958f3dadc972407e8754961bb3317272b8
zhifenghao123 <zhifenghao123@outlook.com>
2023-12-08 10:39:13 +0800

    新增多数据源业务参数

---
+++ how-dev-manage/src/main/java/com/howdev/manage/vo/MutilDataSourceBizParam.java
@@ -0,0 +1,22 @@
+package com.howdev.manage.vo;
+
+/**
+ * MutilDataSourceBizParam class
+ *
+ * @author haozhifeng
+ * @date 2023/12/08
+ */
+public class MutilDataSourceBizParam {
+    /**
+     * 租户/业务线
+     */
+    private String bizLine;
+
+    public String getBizLine() {
+        return bizLine;
+    }
+
+    public void setBizLine(String bizLine) {
+        this.bizLine = bizLine;
+    }
+}
