bbefb96d385973e900b7a8691ff3ef1362994e18
zhifenghao123 <zhifenghao123@outlook.com>
2024-01-31 19:34:46 +0800

    异步决策服务池配置

---
+++ how-dev-biz/src/main/java/com/howdev/app/asynctask/AsyncDecisionServiceConfigure.java
@@ -0,0 +1,65 @@
+package com.howdev.app.asynctask;
+
+import com.howdev.api.model.BaseResponse;
+import com.howdev.app.asynctask.task.InitialAsyncDecisionTask;
+import com.howdev.app.asynctask.task.NonInitialAsyncDecisionTask;
+import com.howdev.app.asynctask.task.RetryAsyncDecisionTask;
+import com.howdev.app.po.AsyncDecisionTaskPo;
+import com.howdev.framework.async.service.AsyncTaskService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * AsyncDecisionTaskConfigure class
+ *
+ * @author haozhifeng
+ * @date 2024/01/26
+ */
+@Component
+public class AsyncDecisionServiceConfigure {
+    /**
+     * 空闲任务等待时间，单位毫秒
+     */
+    @Value("${async.decision.initial-decision.thread.pool.idle-wait-time:200}")
+    private int initialDecisionIdleWaitTime;
+
+    @Value("${async.decision.non-initial-decision.thread.pool.idle-wait-time:200}")
+    private int nonInitialDecisionIdleWaitTime;
+
+    @Value("${async.decision.retry-decision.thread.pool.idle-wait-time:200}")
+    private int retryDecisionIdleWaitTime;
+
+    @Bean
+    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> initialAsyncDecisionTaskService(
+            ThreadPoolExecutor initialDecisionThreadPool, InitialAsyncDecisionTask initialAsyncDecisionTask) {
+        return new AsyncTaskService<>(
+                "initialAsyncDecisionTaskService",
+                initialDecisionThreadPool,
+                initialAsyncDecisionTask,
+                initialDecisionIdleWaitTime);
+    }
+
+    @Bean
+    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> nonInitialAsyncDecisionTaskService(
+            ThreadPoolExecutor nonInitialDecisionThreadPool, NonInitialAsyncDecisionTask nonInitialAsyncDecisionTask) {
+        return new AsyncTaskService<>(
+                "nonInitialAsyncDecisionTaskService",
+                nonInitialDecisionThreadPool,
+                nonInitialAsyncDecisionTask,
+                nonInitialDecisionIdleWaitTime);
+    }
+
+    @Bean
+    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> retryAsyncDecisionTaskService(
+            ThreadPoolExecutor retryDecisionThreadPool, RetryAsyncDecisionTask retryAsyncDecisionTask) {
+        return new AsyncTaskService<>(
+                "retryAsyncDecisionTaskService",
+                retryDecisionThreadPool,
+                retryAsyncDecisionTask,
+                retryDecisionIdleWaitTime);
+    }
+
+}
