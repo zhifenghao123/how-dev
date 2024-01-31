b93905f2e38064fd6593d063a7f2dc33b3fe4fb2
zhifenghao123 <zhifenghao123@outlook.com>
2024-01-31 20:20:02 +0800

    新增异步决策任务Service

---
+++ how-dev-biz/src/main/java/com/howdev/app/service/AsyncDecisionTaskService.java
@@ -0,0 +1,13 @@
+package com.howdev.app.service;
+
+import com.howdev.app.po.AsyncDecisionTaskPo;
+
+/**
+ * AsyncDecisionTaskService class
+ *
+ * @author haozhifeng
+ * @date 2024/01/31
+ */
+public interface AsyncDecisionTaskService {
+
+}
---
+++ how-dev-biz/src/main/java/com/howdev/app/service/impl/AsyncDecisionTaskServiceImpl.java
@@ -0,0 +1,15 @@
+package com.howdev.app.service.impl;
+
+import com.howdev.app.po.AsyncDecisionTaskPo;
+import com.howdev.app.service.AsyncDecisionTaskService;
+import org.springframework.stereotype.Service;
+
+/**
+ * AsyncDecisionTaskServiceImpl class
+ *
+ * @author haozhifeng
+ * @date 2024/01/31
+ */
+@Service
+public class AsyncDecisionTaskServiceImpl implements AsyncDecisionTaskService {
+}
