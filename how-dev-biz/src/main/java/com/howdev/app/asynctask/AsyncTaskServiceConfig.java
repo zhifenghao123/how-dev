31d81efadef8f53e6b597b4f78dae8fc3626b6fe
zhifenghao123 <zhifenghao123@outlook.com>
2024-02-01 10:46:09 +0800

    修改异步任务服务属性配置

--- how-dev-biz/src/main/java/com/howdev/app/asynctask/AsyncTaskServiceConfig.java
+++ how-dev-biz/src/main/java/com/howdev/app/asynctask/AsyncTaskServiceConfig.java
@@ -1,9 +1,9 @@
 package com.howdev.app.asynctask;
 
 import com.howdev.api.model.BaseResponse;
-import com.howdev.app.asynctask.task.InitialAsyncDecisionTaskHandler;
-import com.howdev.app.asynctask.task.NonInitialAsyncDecisionTaskHandler;
-import com.howdev.app.asynctask.task.RetryAsyncDecisionTaskHandler;
+import com.howdev.app.asynctask.task.InitAsyncTaskHandler;
+import com.howdev.app.asynctask.task.NonInitAsyncTaskHandler;
+import com.howdev.app.asynctask.task.RetryAsyncTaskHandler;
 import com.howdev.app.po.AsyncDecisionTaskPo;
 import com.howdev.framework.async.service.AsyncTaskService;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,47 +19,47 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @date 2024/01/26
  */
 @Component
-public class AsyncDecisionServiceConfigure {
+public class AsyncTaskServiceConfig {
     /**
      * 空闲任务等待时间，单位毫秒
      */
-    @Value("${async.decision.initial-decision.thread.pool.idle-wait-time:200}")
-    private int initialDecisionIdleWaitTime;
+    @Value("${async.decision.init.thread.pool.idle-wait-time:200}")
+    private int initIdleWaitTime;
 
-    @Value("${async.decision.non-initial-decision.thread.pool.idle-wait-time:200}")
-    private int nonInitialDecisionIdleWaitTime;
+    @Value("${async.decision.non-init.thread.pool.idle-wait-time:200}")
+    private int nonInitIdleWaitTime;
 
-    @Value("${async.decision.retry-decision.thread.pool.idle-wait-time:200}")
-    private int retryDecisionIdleWaitTime;
+    @Value("${async.decision.retry.thread.pool.idle-wait-time:200}")
+    private int retryIdleWaitTime;
 
     @Bean
-    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> initialAsyncDecisionTaskService(
-            ThreadPoolExecutor initialDecisionThreadPool, InitialAsyncDecisionTaskHandler initialAsyncDecisionTask) {
+    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> initialAsyncTaskService(
+            ThreadPoolExecutor initialAsyncTaskThreadPool, InitAsyncTaskHandler initAsyncTaskHandler) {
         return new AsyncTaskService<>(
-                "initialAsyncDecisionTaskService",
-                initialDecisionThreadPool,
-                initialAsyncDecisionTask,
-                initialDecisionIdleWaitTime);
+                "initialAsyncTaskService",
+                initialAsyncTaskThreadPool,
+                initAsyncTaskHandler,
+                initIdleWaitTime);
     }
 
     @Bean
-    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> nonInitialAsyncDecisionTaskService(
-            ThreadPoolExecutor nonInitialDecisionThreadPool, NonInitialAsyncDecisionTaskHandler nonInitialAsyncDecisionTask) {
+    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> nonInitialAsyncTaskService(
+            ThreadPoolExecutor nonInitialAsyncTaskThreadPool, NonInitAsyncTaskHandler nonInitAsyncTaskHandler) {
         return new AsyncTaskService<>(
-                "nonInitialAsyncDecisionTaskService",
-                nonInitialDecisionThreadPool,
-                nonInitialAsyncDecisionTask,
-                nonInitialDecisionIdleWaitTime);
+                "nonInitialAsyncTaskService",
+                nonInitialAsyncTaskThreadPool,
+                nonInitAsyncTaskHandler,
+                nonInitIdleWaitTime);
     }
 
     @Bean
-    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> retryAsyncDecisionTaskService(
-            ThreadPoolExecutor retryDecisionThreadPool, RetryAsyncDecisionTaskHandler retryAsyncDecisionTask) {
+    public AsyncTaskService<AsyncDecisionTaskPo, BaseResponse> retryAsyncTaskService(
+            ThreadPoolExecutor retryAsyncTaskThreadPool, RetryAsyncTaskHandler retryAsyncTaskHandler) {
         return new AsyncTaskService<>(
-                "retryAsyncDecisionTaskService",
-                retryDecisionThreadPool,
-                retryAsyncDecisionTask,
-                retryDecisionIdleWaitTime);
+                "retryAsyncTaskService",
+                retryAsyncTaskThreadPool,
+                retryAsyncTaskHandler,
+                retryIdleWaitTime);
     }
 
 }
