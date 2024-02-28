eeda7823f69263f9b7a0725fc001fde988a82f35
zhifenghao123 <zhifenghao123@outlook.com>
2024-02-28 12:38:20 +0800

    使用Redis实现分布式锁

---
+++ how-dev-test/src/main/java/com/howdev/test/business/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.howdev.test.business.controller;
+
+import com.howdev.test.redis.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * TestController class
+ *
+ * @author haozhifeng
+ * @date 2024/02/28
+ */
+@Controller
+@RequestMapping("redistest")
+public class TestController {
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping("lock")
+    @ResponseBody
+    public Boolean lock() {
+         Boolean result = redisService.lock("name", "haozhi", 100);
+        return result;
+    }
+
+    @RequestMapping("unlock")
+    @ResponseBody
+    public Boolean unlock() {
+        Boolean result = redisService.unlock("name", "haozhifeng");
+        return result;
+    }
+}
